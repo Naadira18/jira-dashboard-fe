36dd7e8261543212dd4e6776cbc18be78171dfb4
Naadira18 <naadirasahar.n@cprime.com>
2025-07-24 12:25:43 +0530

    updated be url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const prod_be_url = "https://jira-dashboard-be-coral.vercel.app/api/v1"
+export const prod_be_url = "https://fe-dashborad2-be.vercel.app/api/v1"
 // export const prod_be_url = "http://localhost:8000/api/v1"
